c76c930fdb84e861c97f69c5eda827858fe3efc0
sebastian <sebastian.gallardo2307@gmail.com>
2025-08-13 08:59:21 -0400

    Update Day 1 Exercise 1 v2

---
+++ Dia 1/Ejercicio1_v2.py
@@ -0,0 +1,52 @@
+# Respuesta Dia 1 Advent of Code 2024 "Histeria del historiador"
+
+# Parte 1
+# Paso 1 Dividir las filas en dos listas
+archivo = open("datos.txt") # Leer los datos de entrada
+lista1, lista2 = [], []
+for linea in archivo:
+    dato = linea.split() # input
+    lista1.append(dato[0]) # lista de la izquierda
+    lista2.append(dato[1]) # lista de la derecha
+archivo.close()
+
+# Paso 2 Ordenar las listas de menor a mayor
+lista1.sort() 
+lista2.sort()
+
+# Paso 3 Calcular las distancias
+def calcular_distancia(lista1, lista2):
+    distancias = []
+    #Convertir los valores de la lista a enteros con map
+    lista1 = list(map(int, lista1)) 
+    lista2 = list(map(int, lista2))
+    for pos in range(1000):
+        if lista1[pos] >= lista2[pos]:
+            resultado = lista1[pos] - lista2[pos]
+        else:
+            resultado = lista2[pos] - lista1[pos]
+        distancias.append(resultado)
+    return distancias
+
+resultado_distancias = calcular_distancia(lista1, lista2)
+
+# Paso 4 Calcular la distancia total entre ambas listas
+total_distancias = sum(resultado_distancias)
+print(f"La distancia total entre ambas listas es: {total_distancias}")
+
+# Parte 2
+# Paso 1 Averiguar con qué frecuencia aparece cada número de la lista izquierda en la lista derecha.
+def calcular_similitud(lista1, lista2):
+    similitudes = []
+    lista1 = list(map(int, lista1))
+    lista2 = list(map(int, lista2))
+    for valor in lista1:
+        frecuencia = valor * lista2.count(valor)
+        # print(f"la frecuencia de {valor} es: {frecuencia}")
+        similitudes.append(frecuencia)
+    return similitudes
+
+# Paso 2 Calcular la similitud total entre ambas listas
+resultado_similitudes = calcular_similitud(lista1, lista2)
+total_similitud = sum(resultado_similitudes)
+print(f"La similitud total entre ambas listas es: {total_similitud}")
